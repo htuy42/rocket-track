ddfaff3c00a914ba3c96072a7b93a7441dd6ac1d
Josh Pattiz <jpattiz@google.com>
2021-06-01 16:52:41 -1000

    try actix

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
-use rocket::{self, get, routes};
-use rand::Rng;
+use actix_web::{server, App, HttpRequest, Responder};
+use std::env;
 
 
-/// Declare a handler.
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn greet(req: &HttpRequest) -> impl Responder {
+    let to = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", to)
 }
 
-/// Declare a handler.
-#[get("/yell")]
-fn loud_index() -> &'static str {
-    "HELLO, WORLD!"
-}
+// /// Declare a handler.
+// #[get("/yell")]
+// fn loud_index() -> &'static str {
+//     "HELLO, WORLD!"
+// }
 
 // /// Declare a handler.
 // #[get("/rand/<max>")]
@@ -24,7 +21,20 @@ fn loud_index() -> &'static str {
 // }
 
 
-/// Start our server.
 fn main() {
-    rocket::ignite().mount("/", routes![index, loud_index]).launch();
+    // Get the port number to listen on.
+    let port = env::var("PORT")
+    .unwrap_or_else(|_| "3000".to_string())
+    .parse()
+    .expect("PORT must be a number");
+
+    // Start a server, configuring the resources to serve.
+    server::new(|| {
+    App::new()
+        .resource("/", |r| r.f(greet))
+        .resource("/{name}", |r| r.f(greet))
+    })
+    .bind(("0.0.0.0", port))
+    .expect("Can not bind to port 8000")
+    .run();
 }
