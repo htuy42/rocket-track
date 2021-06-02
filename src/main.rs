5169b10d712f785c3c554567527270e3bed6557a
Josh Pattiz <jpattiz@google.com>
2021-06-01 16:25:26 -1000

    comment random

--- src/main.rs
+++ src/main.rs
@@ -16,15 +16,15 @@ fn loudIndex() -> &'static str {
     "HELLO, WORLD!"
 }
 
-/// Declare a handler.
-#[get("/rand/<max>")]
-fn random(max : u8) -> String {
-    let random_number = rand::thread_rng().gen_range(1..max);
-    return format!("Hello for the {}'th time" + random_number);
-}
+// /// Declare a handler.
+// #[get("/rand/<max>")]
+// fn random(max : u8) -> String {
+//     let random_number = rand::thread_rng().gen_range(1..max);
+//     return format!("Hello for the {}'th time" + random_number);
+// }
 
 
 /// Start our server.
 fn main() {
-    rocket::ignite().mount("/", routes![index, loudIndex, random]).launch();
+    rocket::ignite().mount("/", routes![index, loudIndex]).launch();
 }
