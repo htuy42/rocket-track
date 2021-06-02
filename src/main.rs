05554cd56cbab4e48d0b697bfa0c89d62574753b
Josh Pattiz <jpattiz@google.com>
2021-06-01 16:11:18 -1000

    updates to cargo file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-//! A simple Rocket application, based on the example in [Getting Started][].
-//!
-//! [Getting Started]: https://rocket.rs/v0.4/guide/getting-started/
-
 #![feature(proc_macro_hygiene, decl_macro)]
 
 use rocket::{self, get, routes};
